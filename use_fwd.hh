a9a2f4b599dcd49fb26439c1c6e2760ca3ebc86b
sjoyeux <tailor@localhost.localdomain>
2005-10-12 11:13:37 +0000

    No commit message

---
+++ use_fwd.hh
@@ -0,0 +1,26 @@
+/* -*- C++ -*-
+ * $Id: use_fwd.hh 949 2005-02-11 14:51:00Z fpy $
+ */
+#ifndef UTILMM_SINGLETON_USE_FWD
+#  define UTILMM_SINGLETON_USE_FWD
+
+namespace utilmm {
+  namespace singleton {
+
+    template<typename Ty>
+    struct use;
+
+  } // namespace utilmm::singleton
+} // namespace utilmm
+
+#endif // UTILMM_SINGLETON_USE_FWD
+
+/** @file singleton/use_fwd.hh
+ * @brief Forward declaration of @c utilmm::singleton::use
+ *
+ * @sa use.hh
+ *
+ * @author Frédéric Py <fpy@laas.fr>
+ *
+ * @ingroup singleton
+ */
---
+++ wrapper_fwd.hh
@@ -0,0 +1,26 @@
+/* -*- C++ -*-
+ * $Id: wrapper_fwd.hh 949 2005-02-11 14:51:00Z fpy $
+ */
+#ifndef UTILMM_SINGLETON_WRAPPER_FWD
+# define UTILMM_SINGLETON_WRAPPER_FWD
+
+namespace utilmm {
+  namespace singleton {
+
+    template<typename Ty>
+    class wrapper;
+
+  } // namespace utilmm::singleton
+} // namespace utilmm
+
+#endif // UTILMM_SINGLETON_WRAPPER_FWD
+
+/** @file singleton/wrapper_fwd.hh
+ * @brief Forward declaration of @c utilmm::singleton::wrapper
+ *
+ * @sa wrapper.hh
+ *
+ * @author Frédéric Py <fpy@laas.fr>
+ *
+ * @ingroup singleton
+ */
