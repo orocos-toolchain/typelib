f3c74c5459445b54c426ce5449202d5ecd4fa511
sjoyeux <tailor@localhost.localdomain>
2005-10-12 09:07:03 +0000

    No commit message

--- lang/cimport/import.hh
+++ lang/cimport/import.hh
@@ -6,6 +6,13 @@
 class CImport : public Typelib::Importer
 {
 public:
+    /** Load the C file at \c path into \c registry
+     * The available options are:
+     * <ul>
+     *  <li> \c defines the define arguments to cpp (-D options)
+     *  <li> \c includes the include arguments to cpp (-I options)
+     * </ul>
+     */
     virtual bool load
         (std::string const& path
         , utilmm::config_set const& config
