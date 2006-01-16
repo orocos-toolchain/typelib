74c3d08b86021ab4e2d403a899d087bfc1c96dad
sjoyeux <tailor@localhost.localdomain>
2006-01-16 17:58:27 +0000

    No commit message

--- test_suite/test_lang_c.cc
+++ test_suite/test_lang_c.cc
@@ -4,6 +4,7 @@
 #include <typelib/importer.hh>
 #include <typelib/typemodel.hh>
 #include <typelib/registry.hh>
+#include "test_cimport.1"
 using namespace Typelib;
 
 class TC_CImport
@@ -31,10 +32,20 @@ public:
         BOOST_REQUIRE( registry.has("/B") );
 
         // Check that the size of B.a is the same as A
-        Compound const* B   = static_cast<Compound const*>(registry.get("/B"));
-        Field  const* B_a = B->getField("a");
-        BOOST_REQUIRE(B);
-        BOOST_REQUIRE_EQUAL( &(B_a->getType()), registry.get("/ADef") );
+        Compound const* b   = static_cast<Compound const*>(registry.get("/B"));
+        Field  const* b_a = b->getField("a");
+        BOOST_REQUIRE_EQUAL( &(b_a->getType()), registry.get("/ADef") );
+
+        Field  const* b_c = b->getField("c");
+        BOOST_REQUIRE_EQUAL( &(b_c->getType()), registry.get("/float[100]") );
+        BOOST_REQUIRE_EQUAL( b_c->getType().getCategory(), Type::Array );
+
+        Array const& b_c_array(dynamic_cast<Array const&>(b_c->getType()));
+        BOOST_REQUIRE_EQUAL( &b_c_array.getIndirection(), registry.get("/float") );
+
+        BOOST_REQUIRE_EQUAL( registry.get("/struct A")->getSize(), sizeof(A) );
+        BOOST_REQUIRE_EQUAL( b->getSize(), sizeof(B) );
+        BOOST_REQUIRE_EQUAL( b_c_array.getDimension(), 100UL );
     }
 };
 
