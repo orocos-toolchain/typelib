2ad84a40e66990d7ea6ff56f8a2239e6d7721de1
sjoyeux <tailor@localhost.localdomain>
2006-02-07 09:35:34 +0000

    No commit message

--- test_suite/testsuite.cc
+++ test_suite/testsuite.cc
@@ -13,14 +13,14 @@ init_unit_test_suite( int argc, char * argv[] ) {
     test_value(ts_core);
     ts->add(ts_core);
 
-    // Check system first, it is used by configfile
-    test_suite* ts_lang_c = BOOST_TEST_SUITE( "Testing C import" );
-    test_lang_c(ts_lang_c);
-    ts->add(ts_lang_c);
+    test_suite* ts_lang = BOOST_TEST_SUITE( "Testing language plugins" );
+    test_lang_tlb(ts_lang);
+    test_lang_c(ts_lang);
+    ts->add(ts_lang);
 
-//    test_suite* ts_value = BOOST_TEST_SUITE( "Testing in-memory value manipulation" );
-//    test_value(ts_value);
-//    ts->add(ts_value);
+    test_suite* ts_display = BOOST_TEST_SUITE( "Testing value display plugins" );
+    test_display(ts_lang);
+    ts->add(ts_display);
 
     return ts;
 }
