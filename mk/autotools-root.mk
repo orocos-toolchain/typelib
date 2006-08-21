346b52fc38badd95bfdf26010afcb39fe9b080f0
tailor <sylvain.joyeux@laas.fr>
2006-08-21 07:49:03 +0000

    No commit message

--- mk/autotools-root.mk
+++ mk/autotools-root.mk
@@ -2,7 +2,7 @@
 # $Id: autotools-root.mk 1393 2006-02-08 08:05:49Z sjoyeux $
 
 distclean: autotools-root-distclean
-autotools-root-distclean:
+autotools-root-distclean: clean
 	rm -f config.status libtool config.log $(CONFIG_OUTPUT_FILES) mk/init.mk Init.make
 
 clean: pch-clean
--- mk/autotools.mk
+++ mk/autotools.mk
@@ -2,7 +2,7 @@
 # $Id: autotools.mk 1022 2005-10-11 12:06:01Z sjoyeux $
 
 distclean: autotools-distclean
-autotools-distclean::
+autotools-distclean:: clean
 	rm -f Makefile
 
 
