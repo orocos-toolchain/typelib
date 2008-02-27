31ab42ec9b3118eb0ec7663fe22cb18ff2346371
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2008-02-27 10:48:50 +0100

    [doc] properly document the behaviour of typename manipulation functions

--- typelib/typename.hh
+++ typelib/typename.hh
@@ -23,14 +23,16 @@ namespace Typelib
     bool isValidTypename(const std::string& name, bool absolute);
     bool isInNamespace(const std::string& type, const std::string& nspace, bool recursive = false);
     
-    /** Returns +name+ as a namespace name, valid to build
-     * an absolute type name by simple concatenation */
+    /** Returns +name+ as a namespace name, valid to build an absolute type
+     * name by simple concatenation */
     std::string getNormalizedNamespace(const std::string& name);
     /** Removes the namespace part of \c full_name and returns it */
     std::string getTypename (const std::string& full_name);
-    /** Returns the namespace part of \c full_name */
+    /** Returns the normalized namespace part of \c full_name. The returned
+     * namespace name starts and ends with the namespace separator '/'. */
     std::string getNamespace(const std::string& full_name);
-    /** Returns the relative part of +name+, relative to the given namespace */
+    /** Returns the relative part of +name+, relative to the given namespace.
+     * The returned name does not have any leading namespace separator. */
     std::string getRelativeName(std::string const& full_name, std::string const& ns);
 };
 
