orocos-toolchain/typelib

commit b43125abbda86f1363fdca7659fd4e67481f6b43

[lang_c] support for array typedefs
  The form
    typedef <type> <alias>[array_size];
  was silently (and wrongly) interpreted as
    typedef <type> <alias>;

  This patch fixes this;

lang/cimport/typesolver.cc
@@ -182,6 +182,22 @@ void TypeSolver::classForwardDeclaration(TypeSpecifier ts, DeclSpecifier ds, con
     CPPParser::classForwardDeclaration(ts, ds, name);
 }
 
+TypeBuilder TypeSolver::initializeTypeBuilder()
+{
+    TypeBuilder builder(m_registry, m_fieldtype);
+    if (pointer_level)
+	builder.addPointer(pointer_level);
+
+    while (!m_pending_array.empty())
+    {
+	builder.addArrayMinor(m_pending_array.front());
+	m_pending_array.pop_front();
+    }
+
+    pointer_level = 0;
+    return builder;
+}
+
 void TypeSolver::declaratorID(const std::string& name, QualifiedItem qi)
 {
     if (m_class && m_class_name.empty() && qi == qiType)
@@ -195,33 +211,24 @@ void TypeSolver::declaratorID(const std::string& name, QualifiedItem qi)
         }
     }
     else if (m_class)
-    {
-        m_fields.push_back( make_pair(name, TypeBuilder(m_registry, m_fieldtype)) );
-        if (pointer_level)
-            m_fields.back().second.addPointer(pointer_level);
-    }
+        m_fields.push_back( make_pair(name, initializeTypeBuilder()) );
     else if (qi == qiType)
     {
-        TypeBuilder builder(m_registry, m_fieldtype);
-        if (pointer_level)
-            builder.addPointer(pointer_level);
-        
+        TypeBuilder builder = initializeTypeBuilder();
         std::string new_name = m_registry.getFullName(name);
         std::string old_name = m_registry.getFullName(builder.getType().getName());
         m_registry.alias( old_name, new_name );
     }
+
+    if (! m_pending_array.empty())
+	std::cerr << "WARNING: the set of pending array dimensions is not empty" << std::endl;
+
     CPPParser::declaratorID(name, qi);
 }
 
 void TypeSolver::declaratorArray(int size)
 {
-    if (m_class && !m_fields.empty())
-    {
-	// C orders dimensions from the outermost
-	// at the left to the innermost at the right
-        m_fields.back().second.addArrayMinor(size);
-    }
-        
+    m_pending_array.push_back(size);
     CPPParser::declaratorArray(size);
 }
 

test/test_cimport.1
@@ -8,6 +8,8 @@ struct A
 };
 
 typedef struct A ADef;
+typedef int array_typedef[256];
+typedef int multi_array_typedef[256][512];
 
 typedef struct B
 {

lang/cimport/cparser.g
@@ -541,13 +541,6 @@ direct_declarator
 	|
 		(qualified_id LSQUARE)=>	// Must be array declaration
 		id = qualified_id
-                { 
-                    if (_td==true)
-			declaratorID(id,qiType);
-		    else
-			declaratorID(id,qiVar);
-		    is_address = false; pointer_level = 0;
-		}
 		(
                     options {warnWhenFollowAmbig = false;}:
 		    LSQUARE { array_size = 0; }
@@ -556,6 +549,13 @@ direct_declarator
                     )   { declaratorArray(array_size); }
                     RSQUARE
                 )+
+                { 
+                    if (_td==true)
+			declaratorID(id,qiType);
+		    else
+			declaratorID(id,qiVar);
+		    is_address = false; pointer_level = 0;
+		}
 	|
 		id = qualified_id
 		{ 

lang/cimport/typesolver.hh
@@ -24,6 +24,7 @@ class TypeSolver : public CPPParser
     typedef std::list< FieldBuilder > FieldList;
     std::list<std::string> m_fieldtype;
     FieldList       m_fields;
+    std::list<size_t> m_pending_array;
 
     // For Enums
     typedef std::list< std::pair<std::string, int> > ValueMap;
@@ -33,6 +34,8 @@ class TypeSolver : public CPPParser
     Typelib::Registry& m_registry;
 
     bool m_cxx_mode;
+
+    Typelib::TypeBuilder initializeTypeBuilder();
 
 public:
     class UnsupportedClassType

test/test_lang_c.cc
@@ -9,6 +9,18 @@
 #include "test_cimport.1"
 using namespace Typelib;
 
+void import_test_types(Registry& registry)
+{
+    static const char* test_file = TEST_DATA_PATH("test_cimport.h");
+
+    utilmm::config_set config;
+    PluginManager::self manager;
+    Importer* importer = manager->importer("c");
+    config.set("include", TEST_DATA_PATH(".."));
+    config.set("define", "GOOD");
+    BOOST_REQUIRE_NO_THROW( importer->load(test_file, config, registry) );
+}
+
 BOOST_AUTO_TEST_CASE( test_strict_c_import )
 {
     static const char* test_file = TEST_DATA_PATH("test_cimport.h");
@@ -155,5 +167,25 @@ BOOST_AUTO_TEST_CASE( test_c_import )
 	BOOST_REQUIRE( it != map.end() );
 	BOOST_CHECK_EQUAL(exp_it->value, it->second);
     }
+}
+
+BOOST_AUTO_TEST_CASE( test_c_array_typedefs )
+{
+    Registry registry;
+    import_test_types(registry);
+
+    BOOST_REQUIRE(( registry.get("array_typedef") ));
+    Array const* array_t = dynamic_cast<Typelib::Array const*>(registry.get("array_typedef"));
+    BOOST_REQUIRE(( array_t ));
+    BOOST_REQUIRE_EQUAL(256, array_t->getDimension());
+    BOOST_REQUIRE_EQUAL(registry.get("int"), &array_t->getIndirection());
+
+    BOOST_REQUIRE(( registry.get("multi_array_typedef") ));
+    array_t = dynamic_cast<Typelib::Array const*>(registry.get("multi_array_typedef"));
+    BOOST_REQUIRE(( array_t ));
+    BOOST_REQUIRE_EQUAL(256, array_t->getDimension());
+    array_t = dynamic_cast<Typelib::Array const*>(&array_t->getIndirection());
+    BOOST_REQUIRE_EQUAL(512, array_t->getDimension());
+    BOOST_REQUIRE_EQUAL(registry.get("int"), &array_t->getIndirection());
 }
 

commit b88ba42c9fda671d4fbfcb55354436631a412bf3

[lang_c] properly ignore function definitions
  b43125abbda86f1363fdca7659fd4e67481f6b43 introduced a bug because
  pending array dimensions could be added during the parsing of function
  definitions. Since we ignore function definitions, the pending
  dimension was kept and used for the next type definition.

  This commit fixes it by clearing the pending dimensions array when a
  declaratorID is found, which is not a type definition.

lang/cimport/typesolver.cc
@@ -219,9 +219,23 @@ void TypeSolver::declaratorID(const std::string& name, QualifiedItem qi)
         std::string old_name = m_registry.getFullName(builder.getType().getName());
         m_registry.alias( old_name, new_name );
     }
+    else
+    {
+        // we were parsing either a function definition or an argument
+        // definition. In that case, just ignore the pending array values.
+        m_pending_array.clear();
+    }
 
     if (! m_pending_array.empty())
+    {
 	std::cerr << "WARNING: the set of pending array dimensions is not empty" << std::endl;
+	std::cerr << "WARNING: name == " << name << ", qiType == " << qiType << std::endl;
+        std::cerr << "WARNING: got";
+        list<size_t>::const_iterator it, end = m_pending_array.end();
+        for (it = m_pending_array.begin(); it != end; ++it)
+            std::cerr << " " << *it;
+        std::cerr << std::endl;
+    }
 
     CPPParser::declaratorID(name, qi);
 }

test/test_cimport.1
@@ -1,3 +1,5 @@
+int h2RecordErrMsgs(char *bywho, char *moduleName, short moduleId, 
+		    int nbErrors, const int errMsgs[]);
 
 struct A
 {