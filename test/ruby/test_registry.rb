c56f314c5c9a24502d1dc3b719b3d2422ae37e9f
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2008-11-19 16:43:21 +0100

    test,ruby: fix XML validation
    The current XML validation was actually not testing what
    it was supposed to, because the test XML input was invalid.

--- test/ruby/test_registry.rb
+++ test/ruby/test_registry.rb
@@ -5,13 +5,13 @@ class TC_Registry < Test::Unit::TestCase
         test = "malformed_xml"
         assert_raises(ArgumentError) { Typelib::Registry.from_xml(test) }
 
-        test = "<typelib><invalid_element name=\"name\" size=0 /></typelib>"
+        test = "<typelib><invalid_element name=\"name\" size=\"0\" /></typelib>"
         assert_raises(ArgumentError) { Typelib::Registry.from_xml(test) }
 
         test = "<typelib><opaque name=\"name\" /></typelib>"
         assert_raises(ArgumentError) { Typelib::Registry.from_xml(test) }
 
-        test = "<typelib><opaque name=\"invalid type name\" size=0 /></typelib>"
+        test = "<typelib><opaque name=\"invalid type name\" size=\"0\" /></typelib>"
         assert_raises(ArgumentError) { Typelib::Registry.from_xml(test) }
     end
 end
