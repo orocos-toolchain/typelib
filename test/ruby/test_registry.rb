16921576524678d923b1972959516623f27bef53
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2008-11-20 17:57:20 +0100

    test,ruby: move the registry-related tests from test_type to test_registry

--- test/ruby/test_registry.rb
+++ test/ruby/test_registry.rb
@@ -1,6 +1,53 @@
 require 'test_config'
 
 class TC_Registry < Test::Unit::TestCase
+    def test_aliasing
+	registry = Registry.new
+	registry.alias "/my_own_and_only_int", "/int"
+	assert_equal(registry.get("my_own_and_only_int"), registry.get("int"))
+	assert_equal("/int", registry.get("/my_own_and_only_int").name)
+    end
+
+    def test_registry
+        assert_raises(RuntimeError) { Registry.guess_type("bla.1") }
+        assert_equal("c", Registry.guess_type("blo.c"))
+        assert_equal("c", Registry.guess_type("blo.h"))
+        assert_equal("tlb", Registry.guess_type("blo.tlb"))
+
+        assert_raises(RuntimeError) { Registry.new.import("bla.c") }
+
+        registry = Registry.new
+        testfile = File.join(SRCDIR, "test_cimport.h")
+        assert_raises(RuntimeError) { registry.import(testfile) }
+        assert_nothing_raised {
+            registry.import(testfile, nil, :include => [ File.join(SRCDIR, '..') ], :define => [ 'GOOD' ])
+        }
+
+        registry = Registry.new
+        assert_nothing_raised {
+            registry.import(testfile, nil, :rawflag => [ "-I#{File.join(SRCDIR, '..')}", "-DGOOD" ])
+        }
+        assert_nothing_raised {
+            registry.import(testfile, nil, :merge => true, :rawflag => [ "-I#{File.join(SRCDIR, '..')}", "-DGOOD" ])
+        }
+	assert_raises(RuntimeError) { registry.import(testfile, nil, :rawflag => [ "-I#{File.join(SRCDIR, '..')}", "-DGOOD" ]) }
+    end
+
+    def test_registry_iteration
+	reg = make_registry
+	values = nil
+	assert_nothing_raised { values = reg.enum_for(:each_type).to_a }
+
+	assert_not_equal(0, values.size)
+	assert(values.include?(reg.get("/int")))
+	assert(values.include?(reg.get("/struct EContainer")))
+
+	assert_nothing_raised { values = reg.enum_for(:each_type, true).to_a }
+	assert_not_equal(0, values.size)
+	assert(values.include?(["/struct EContainer", reg.get("/struct EContainer")]))
+	assert(values.include?(["/EContainer", reg.get("/EContainer")]))
+    end
+
     def test_validate_xml
         test = "malformed_xml"
         assert_raises(ArgumentError) { Typelib::Registry.from_xml(test) }
--- test/ruby/test_type.rb
+++ test/ruby/test_type.rb
@@ -22,53 +22,6 @@ class TC_Type < Test::Unit::TestCase
         registry
     end
 
-    def test_aliasing
-	registry = Registry.new
-	registry.alias "/my_own_and_only_int", "/int"
-	assert_equal(registry.get("my_own_and_only_int"), registry.get("int"))
-	assert_equal("/int", registry.get("/my_own_and_only_int").name)
-    end
-
-    def test_registry
-        assert_raises(RuntimeError) { Registry.guess_type("bla.1") }
-        assert_equal("c", Registry.guess_type("blo.c"))
-        assert_equal("c", Registry.guess_type("blo.h"))
-        assert_equal("tlb", Registry.guess_type("blo.tlb"))
-
-        assert_raises(RuntimeError) { Registry.new.import("bla.c") }
-
-        registry = Registry.new
-        testfile = File.join(SRCDIR, "test_cimport.h")
-        assert_raises(RuntimeError) { registry.import(testfile) }
-        assert_nothing_raised {
-            registry.import(testfile, nil, :include => [ File.join(SRCDIR, '..') ], :define => [ 'GOOD' ])
-        }
-
-        registry = Registry.new
-        assert_nothing_raised {
-            registry.import(testfile, nil, :rawflag => [ "-I#{File.join(SRCDIR, '..')}", "-DGOOD" ])
-        }
-        assert_nothing_raised {
-            registry.import(testfile, nil, :merge => true, :rawflag => [ "-I#{File.join(SRCDIR, '..')}", "-DGOOD" ])
-        }
-	assert_raises(RuntimeError) { registry.import(testfile, nil, :rawflag => [ "-I#{File.join(SRCDIR, '..')}", "-DGOOD" ]) }
-    end
-
-    def test_registry_iteration
-	reg = make_registry
-	values = nil
-	assert_nothing_raised { values = reg.enum_for(:each_type).to_a }
-
-	assert_not_equal(0, values.size)
-	assert(values.include?(reg.get("/int")))
-	assert(values.include?(reg.get("/struct EContainer")))
-
-	assert_nothing_raised { values = reg.enum_for(:each_type, true).to_a }
-	assert_not_equal(0, values.size)
-	assert(values.include?(["/struct EContainer", reg.get("/struct EContainer")]))
-	assert(values.include?(["/EContainer", reg.get("/EContainer")]))
-    end
-
     def test_type_inequality
         # Check that == returns false when the two objects aren't of the same class
         # (for instance type == nil shall return false)
@@ -96,7 +49,7 @@ class TC_Type < Test::Unit::TestCase
         type     = registry.get("/Bla/Blo</Template>")
         assert_equal("/Bla/", type.namespace)
         assert_equal("Blo</Template>", type.basename)
-
+        assert_equal("::Bla::Blo<::Template>", type.full_name("::", false))
     end
 
     def test_pointer
