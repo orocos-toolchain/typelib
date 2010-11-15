af060b9080966955ede9be5a460de442a994749f
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2010-11-15 15:35:29 +0100

    fix handling of templates in IDL type generation

--- lang/idl/export.cc
+++ lang/idl/export.cc
@@ -16,7 +16,7 @@ namespace
     {
         int template_mark;
         string result = name;
-        while ((template_mark = result.find_first_of("<>")) != string::npos)
+        while ((template_mark = result.find_first_of("<>/,")) != string::npos)
             result.replace(template_mark, 1, "_");
         return result;
     }
