92e208905781f2d6b6f30be6979b25f688690c54
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2011-04-21 11:57:31 +0200

    ruby: implement a scheme to allow subtype access in specialize blocks
    See documentation

--- bindings/ruby/lib/typelib.rb
+++ bindings/ruby/lib/typelib.rb
@@ -97,6 +97,78 @@ module Typelib
     end
     @use_dynamic_wrappers = false
 
+
+    # Internal proxy class that is used to offer a nice way to get hold on types
+    # in #specialize blocks.
+    #
+    # The reason of the existence of that class is that, in principle,
+    # specialize block should not rely on the global availability of a type,
+    # i.e. they should not rely on the fact that the underlying type registry
+    # has been exported in a particular namespace.
+    #
+    # However, it also means that in every method in specialize blocks, one
+    # would have to do type tricks like
+    #
+    #   self['field_name'].deference.deference
+    #
+    # which is not nice.
+    #
+    # This class is used so that the following scheme is possible instead:
+    #
+    #   Typelib.specialize type_name do
+    #      Subtype = self['field_name']
+    #
+    #      def my_method
+    #        Subtype.new
+    #      end
+    #   end
+    #
+    class TypeSpecializationModule < Module # :nodoc:
+        def included(obj)
+            @base_type = obj
+        end
+
+        class TypeDefinitionAccessor # :nodoc:
+            def initialize(specialization_module, ops)
+                @specialization_module = specialization_module
+                @ops = ops
+            end
+
+            def deference
+                TypeDefinitionAccessor.new(@specialization_module, @ops + [[:deference]])
+            end
+
+            def [](name)
+                TypeDefinitionAccessor.new(@specialization_module, @ops + [[:[], name]])
+            end
+
+            def method_missing(*mcall, &block)
+                if !@type
+                    base_type = @specialization_module.instance_variable_get(:@base_type)
+                    if base_type
+                        @type = @ops.inject(base_type) do |type, m|
+                            type.send(*m)
+                        end
+                    end
+                end
+
+                if !@type
+                    super
+                else
+                    @type.send(*mcall, &block)
+                end
+            end
+        end
+
+        def deference
+            TypeDefinitionAccessor.new(self, [[:deference]])
+        end
+
+        def [](name)
+            TypeDefinitionAccessor.new(self, [[:[], name]])
+        end
+    end
+
     # Extends instances of a given Typelib type
     #
     # This method allows to add methods that are then available on Typelib
@@ -124,7 +196,7 @@ module Typelib
     def self.specialize(name, options = Hash.new, &block)
         options = Kernel.validate_options options, :if => lambda { |t| true }
         value_specializations[name] ||= Array.new
-        value_specializations[name] << [options, Module.new(&block)]
+        value_specializations[name] << [options, TypeSpecializationModule.new(&block)]
     end
 
     # Declares how to convert values of the given type to an equivalent Ruby
