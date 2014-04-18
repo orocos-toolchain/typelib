fd97fef64ad756e27379446ebe65d632d5e49164
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2014-04-18 16:20:38 +0200

    remove the old and unused antlr-based C++ parser
    It has been long superseded by the gccxml importer, and -- quite
    honestly -- probably does not compile anymore

--- lang/cimport/plugin.cc
+++
@@ -1,9 +0,0 @@
-#include "import.hh"
-#include <typelib/plugins.hh>
-#include <typelib/registry.hh>
-
-extern "C" void registerPlugins(Typelib::PluginManager& manager)
-{
-    manager.add(new Typelib::GenericIOPlugin<CImport>("c"));
-}
-
