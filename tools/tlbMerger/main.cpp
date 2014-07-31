41cf132e64041a0d28bc74f081bc69d6374fadf4
Janosch Machowinski <janosch.machowinski@dfki.de>
2014-07-31 17:12:55 +0200

    Added tool for merging two tlb registries

---
+++ tools/tlbMerger/main.cpp
@@ -0,0 +1,34 @@
+#include "../../lang/tlb/export.hh"
+#include "../../lang/tlb/import.hh"
+
+#include <iostream>
+#include <fstream>
+
+int main(int argc, const char **argv) {
+
+    if(argc != 4)
+    {
+        std::cout << "Usage : tlbMerge Input1 Input2 Output" << std::endl;
+        return 0;
+    }
+
+    std::string input1(argv[1]);
+    std::string input2(argv[2]);
+    std::string output(argv[3]);
+
+    Typelib::Registry reg1;
+    Typelib::Registry reg2;
+    
+    TlbImport importer;
+    
+    importer.load(input1, utilmm::config_set(), reg1);
+    importer.load(input2, utilmm::config_set(), reg2);
+    
+    reg1.merge(reg2);
+
+    TlbExport exporter;
+    exporter.save(output, utilmm::config_set(), reg1);
+  
+    return 0;
+  
+}
