46313d2a22ee316173f34c7eea7d51e1c107b1b8
Martin Zenzes <martin.zenzes@dfki.de>
2014-08-04 17:32:55 +0200

    tlbBuilder: test-header-data: add new "strings.h" and the current tlb of eigen.h
    does not look sooo smooth ;-)
    
    Signed-off-by: Martin Zenzes <martin.zenzes@dfki.de>

---
+++ tools/tlbBuilder/test-header-data/strings.h
@@ -0,0 +1,18 @@
+#ifndef TEST_HEADER_DATA_STRINGS_H
+#define TEST_HEADER_DATA_STRINGS_H
+
+#include <string>
+
+namespace strings {
+
+    struct S1 {
+        std::string myS;
+    };
+
+    struct S2 {
+        std::wstring W;
+    };
+
+}
+
+#endif /*TEST_HEADER_DATA_STRINGS_H*/
