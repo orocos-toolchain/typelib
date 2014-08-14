e172c215a1d0d50c24d7609240b30e37d2f12894
Martin Zenzes <martin.zenzes@dfki.de>
2014-08-14 10:28:07 +0200

    tools: rename "tlbMerger" to "typelib-tlb-merger"
    plus some minor adjustments in the "usage" print
    
    Signed-off-by: Martin Zenzes <martin.zenzes@dfki.de>

--- tools/typelib-tlb-merger/typelib-tlb-merger.cpp
+++ tools/typelib-tlb-merger/typelib-tlb-merger.cpp
@@ -8,7 +8,8 @@ int main(int argc, const char **argv) {
 
     if(argc != 4)
     {
-        std::cout << "Usage : tlbMerge Input1 Input2 Output" << std::endl;
+        std::cerr << "Usage : " << argv[0]
+                  << " <Input1.tlb> <Input2.tlb> <Output.tlb>" << std::endl;
         return 0;
     }
 
@@ -18,17 +19,17 @@ int main(int argc, const char **argv) {
 
     Typelib::Registry reg1;
     Typelib::Registry reg2;
-    
+
     TlbImport importer;
-    
+
     importer.load(input1, utilmm::config_set(), reg1);
     importer.load(input2, utilmm::config_set(), reg2);
-    
+
     reg1.merge(reg2);
 
     TlbExport exporter;
     exporter.save(output, utilmm::config_set(), reg1);
-  
+
     return 0;
-  
+
 }
