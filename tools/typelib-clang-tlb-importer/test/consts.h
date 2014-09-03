e2a9546a26895aadf1955058a37a984e9e19cdef
Martin Zenzes <martin.zenzes@dfki.de>
2014-09-03 18:09:32 +0200

    clang-tlb-importer: deactivate consts.h
    causes an error currently...
    
    Signed-off-by: Martin Zenzes <martin.zenzes@dfki.de>

--- tools/typelib-clang-tlb-importer/test/consts.h
+++ tools/typelib-clang-tlb-importer/test/consts.h
@@ -3,22 +3,22 @@
 
 namespace consts {
 
-    typedef long unsigned int my_typedef;
-    typedef long unsigned int my_other_typedef;
+    /* typedef long unsigned int my_typedef; */
+    /* typedef long unsigned int my_other_typedef; */
 
-    typedef const long unsigned int my_const_typedef;
-    typedef long unsigned const int my_other_const_typedef;
+    /* typedef const long unsigned int my_const_typedef; */
+    /* typedef long unsigned const int my_other_const_typedef; */
 
-    struct C1 {
-        my_const_typedef m1;
-        my_other_const_typedef m2;
-        my_other_typedef m3;
-    };
+    /* struct C1 { */
+    /*     my_const_typedef m1; */
+    /*     my_other_const_typedef m2; */
+    /*     my_other_typedef m3; */
+    /* }; */
 
-    struct C2 {
-        const int *m1;
-        const double m2;
-    };
+    /* struct C2 { */
+    /*     const int *m1; */
+    /*     const double m2; */
+    /* }; */
 
 }
 
