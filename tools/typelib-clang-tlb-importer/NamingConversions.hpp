1468b934db01adb8ad4e597f012e71060a60e7ef
Martin Zenzes <martin.zenzes@dfki.de>
2014-09-03 18:12:05 +0200

    clang-tlb-import: completely rework NamingConversions
    left no stone on the other...
    
    template-tokenization is now done based on clang-type informations
    instead of string-manipulation. makes the behaviour a lot more stable on
    "weird" (but legal) variable naming.
    
    Signed-off-by: Martin Zenzes <martin.zenzes@dfki.de>

--- tools/typelib-clang-tlb-importer/NamingConversions.hpp
+++ tools/typelib-clang-tlb-importer/NamingConversions.hpp
@@ -1,15 +1,30 @@
 #ifndef NAMINGCONVERSIONS_H
 #define NAMINGCONVERSIONS_H
 
+#include <clang/AST/Type.h>
+#include <clang/AST/Decl.h>
+
 #include <string>
+#include <vector>
 
-std::string cxxToTyplibName(const std::string& name);
+/** convert name of a c++ decl into typelib syntax
+ *
+ * @param decl object whose name is to be resolved
+ * @return the resolved name of the cxx-type in typelib lingo
+ */
+std::string cxxToTyplibName(const clang::NamedDecl* decl);
 
-/** converts a "typelib" name to a "c++" name
+/** convert name of a c++ type into typelib syntax
+ *
+ * @param type object whose name is to be resolved
+ * @return the resolved name of the cxx-type in typelib lingo
+ */
+std::string cxxToTyplibName(const clang::QualType& type);
+
+/** converts a "typelib" name to a "c++" name. uses some assumptions.
  *
  * for example:
  *    /base/Joints -> base::Joints
- * FIXME: add more concrete examples
  *
  * @param typelibName the name as used in the typelib-side of the portal
  * @return the string converted to c++ lingo
