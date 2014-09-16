29eac0f8ed8f7701100072cc1e0c95ef186a7f48
Martin Zenzes <martin.zenzes@dfki.de>
2014-09-16 17:27:30 +0200

    clang.rb: add "template parsing" stuff from gccxmlrb -- for the time beeing
    not a nice solution, maybe we find something better
    
    Signed-off-by: Martin Zenzes <martin.zenzes@dfki.de>

--- bindings/ruby/lib/typelib/clang.rb
+++ bindings/ruby/lib/typelib/clang.rb
@@ -44,7 +44,79 @@ module Typelib
             
             registry.merge(result)
         end
+        TYPE_HANDLERS['c'] = method(:load_from_clang)
+    end
 
-        TYPE_HANDLERS['clang'] = method(:load_from_clang)
+    class ClangLoader
+        def self.parse_template(name)
+            tokens = template_tokenizer(name)
+
+            type_name = tokens.shift
+            arguments = collect_template_arguments(tokens)
+            arguments.map! do |arg|
+                arg.join("")
+            end
+            return type_name, arguments
+        end
+
+        def self.collect_template_arguments(tokens)
+            level = 0
+            arguments = []
+            current = []
+            while !tokens.empty?
+                case tk = tokens.shift
+                when "<"
+                    level += 1
+                    if level > 1
+                        current << "<" << tokens.shift
+                    else
+                        current = []
+                    end
+                when ">"
+                    level -= 1
+                    if level == 0
+                        arguments << current
+                        current = []
+                        break
+                    else
+                        current << ">"
+                    end
+                when ","
+                    if level == 1
+                        arguments << current
+                        current = []
+                    else
+                        current << "," << tokens.shift
+                    end
+                else
+                    current << tk
+                end
+            end
+            if !current.empty?
+                arguments << current
+            end
+
+            return arguments
+        end
+
+        def self.template_tokenizer(name)
+            suffix = name
+            result = []
+            while !suffix.empty?
+                suffix =~ /^([^<,>]*)/
+                match = $1.strip
+                if !match.empty?
+                    result << match
+                end
+                char   = $'[0, 1]
+                suffix = $'[1..-1]
+
+                break if !suffix
+
+                result << char
+            end
+            result
+        end
+
     end
 end
