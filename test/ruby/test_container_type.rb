01083356ae6dc3fcd5418cb08126804712e25479
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2014-10-31 09:43:47 -0200

    test,ruby: #raw_memcpy is part of StdVector, not ContainerType, reorganize the tests

--- test/ruby/test_container_type.rb
+++ test/ruby/test_container_type.rb
@@ -1,6 +1,6 @@
 require 'typelib/test'
 
-describe Typelib::ArrayType do
+describe Typelib::ContainerType do
     attr_reader :registry
     before do
         @registry = Typelib::CXXRegistry.new
@@ -58,29 +58,6 @@ describe Typelib::ArrayType do
         it "returns the pack code as well as the packed data if :pack_simple_arrays is true and the array elements are numerics" do
             v = value.to_simple_value(pack_simple_arrays: true)
             assert_equal ruby_value, Base64.strict_decode64(v[:data]).unpack("#{v[:pack_code]}*")
-        end
-    end
-
-    describe "#raw_memcpy" do
-        attr_reader :value_t
-        before do
-            @value_t = registry.create_container '/std/vector', '/int32_t'
-        end
-
-        it "should successfully copy from a raw address" do
-            v0 = value_t.new
-            v0 << 0 << 1
-            v1 = value_t.new
-            v1.raw_memcpy(v0.raw_get(0).to_memory_ptr.zone_address, 8)
-            assert_equal 2, v1.size
-            assert_equal 0, v1[0]
-            assert_equal 1, v1[1]
-        end
-        it "should validate its size argument to be a fixed number of elements" do
-            v = value_t.new
-            assert_raises(ArgumentError) do
-                v.raw_memcpy(0, 7)
-            end
         end
     end
 end
