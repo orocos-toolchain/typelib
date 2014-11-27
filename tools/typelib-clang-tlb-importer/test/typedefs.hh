89ba88230288aa9ce30971288159f4ad722037b0
Martin Zenzes <martin.zenzes@dfki.de>
2014-11-27 12:52:04 +0100

    clang-tlb-import: renovate some typedef-related testcases
    - fix a compiler-warning upon non-used private variable in "class A"
    - clarify some comments
    
    Signed-off-by: Martin Zenzes <martin.zenzes@dfki.de>

--- tools/typelib-clang-tlb-importer/test/typedefs.hh
+++ tools/typelib-clang-tlb-importer/test/typedefs.hh
@@ -5,14 +5,14 @@
 
 // what should be done here:
 //
-// - we want to be able to use a typedef'ed type (intTypedef, intTypedefTypedef and cClassTypedef)
-//   inside a record
+// - we want to be able to use a typedef'ed type (intTypedef, intTypedefTypedef
+//   and cClassTypedef) inside a record
 //
 
 namespace ns {
 
     // a class which is typedefed
-    class aClass { int A; };
+    class aClass { public: int A; };
     typedef aClass aClassTypedef;
 
     // a simple typedef
@@ -38,26 +38,30 @@ namespace ns {
         float *c; // this would invalidate this struct if it would not be named
                   // as a opaque
     };
-    // this is given as the actual opaque
+    // this is given as the actual opaque, and so we expect that the 'struct
+    // S1' is added to the database
     typedef struct S1 myTestTypedef;
 
-
-    // same here, only that S2 itself is an opaque defined in the database
+    // same here, only that S2 itself (and not a typedef on it) is an opaque
+    // defined in the "typedefs.opaques"
     struct S2 {
         float *a;
     };
 
-    // this wants to test how we proceed with typedefs that are declared, but
-    // don't have actually a place or location where their content is defined
-    typedef struct not_defined testTypedef;
-
-
-    // but we add a second case without instantiation, where we have an opaque
+    // add another case without instantiation: we specified an opaque of
+    // '/ns/C2</float>'in the file "typedefs.opaques" which names an
+    // uninstantiated specialization of the following template-class. the
+    // actual <float>-template is never seen by the compiler and hence no
+    // "source_file_line" is added to the <opaque> entry of the database.
     template<class T>
     class C2 {
         T A;
     };
-}
 
+    // this wants to test how we proceed with typedefs that are declared, but
+    // don't have actually a place or location where their content is defined.
+    typedef struct not_defined testTypedef;
+
+}
 
 #endif /*TEST_HEADER_DATA_TYPEDEFS_H*/
