af48e123176b9f4743bb7c8fafc84f3ccb4c4c15
Martin Zenzes <martin.zenzes@dfki.de>
2014-12-02 12:16:13 +0100

    clang-tlb-import: re-enable "consts.hh", add "consts.tlb"
    did crash before, works now...
    
    "const" qualifiers have the problem that (at least with the
    clang-importer) they are aliased in the typeli-registry to their
    non-const version. thereby circumventing the whole type-annotation
    system of C/C++... but as the type-information is only used to
    copy/transport the data, this is not so bad. it does not make sense to
    have a "const&" as datatype
    
    altough it would be a cool feature: to generate the code somehow and let
    it compile for in-process transports to allow language-based
    zero-copy... whatever...
    
    Signed-off-by: Martin Zenzes <martin.zenzes@dfki.de>

--- tools/typelib-clang-tlb-importer/test/consts.hh
+++ tools/typelib-clang-tlb-importer/test/consts.hh
@@ -3,22 +3,27 @@
 
 namespace consts {
 
-    /* typedef long unsigned int my_typedef; */
-    /* typedef long unsigned int my_other_typedef; */
+    // "const" qualifiers have the problem that (at least with the
+    // clang-importer) they are aliased to their non-const version. thereby
+    // defeating the whole compile-time-safety and type-annotation system of
+    // C/C++...
 
-    /* typedef const long unsigned int my_const_typedef; */
-    /* typedef long unsigned const int my_other_const_typedef; */
+    typedef long unsigned int my_typedef;
+    typedef long unsigned int my_other_typedef;
 
-    /* struct C1 { */
-    /*     my_const_typedef m1; */
-    /*     my_other_const_typedef m2; */
-    /*     my_other_typedef m3; */
-    /* }; */
+    typedef const long unsigned int my_const_typedef;
+    typedef long unsigned const int my_other_const_typedef;
 
-    /* struct C2 { */
-    /*     const int *m1; */
-    /*     const double m2; */
-    /* }; */
+    struct C1 {
+        my_const_typedef m1;
+        my_other_const_typedef m2;
+        my_other_typedef m3;
+    };
+
+    struct C2 {
+        const int *m1;
+        const double m2;
+    };
 
 }
 
