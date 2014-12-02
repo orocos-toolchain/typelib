45e0d6bc57e031ad5d7136ba59516f01b9146787
Martin Zenzes <martin.zenzes@dfki.de>
2014-12-02 12:08:28 +0100

    clang-tlb-import/opaques.tlb: some more testing
    - add bells and whistles to "opaques.hh"
    - add second "templated opaque", with two arguments
    - add instantiations of the templates which should be opaques
    
    Signed-off-by: Martin Zenzes <martin.zenzes@dfki.de>

--- tools/typelib-clang-tlb-importer/test/opaques.hh
+++ tools/typelib-clang-tlb-importer/test/opaques.hh
@@ -9,28 +9,52 @@ namespace ns_opaques {
     };
 
     // this class has a pointer, which should prevent it from beeing added to
-    // the database. however, if we provide a opaques-file where the class is
-    // named, it will still be added.
+    // the database. however, we added an entry into "opaques.opaques" where
+    // the class is named, so it has to be be added.
     class C1 {
+      public:
         float* a_float_pointer;
         struct cruft test;
     };
-
+    // whereas this one has a reference in it and is not named as an opaque. it
+    // should be ignored...
     class C2 {
+      public:
         float& a_float_reference;
         struct cruft test;
     };
 
-    // what works is giving the "typedef_of_C3" as opaque. what works _not_ is giving "C4<float" as opaque
+    // what works is providing "/opaques/typedef_of_C3" as opaque. filling the
+    // metadata entries will be provided by the "typedef" path,
+    // "opaque_is_typedef" for example will be set.
     template<typename T>
     class C3 {
         T* another_float_pointer;
     };
     typedef C3<float> typedef_of_C3;
+
+    // what _not_ work easily is defining "/opaques/C4</float>" as opaque --
+    // the compiler does not see the template. why?
+    //
+    // the opaque is actually never seen in the "opaque lookup phase" where the
+    // code looks for namedDecl's...? what we have to look for are
+    // "ClassTemplateDecl" with the name without template-parms
     template<typename T>
     class C4 {
         T* another_float_pointer;
     };
+    // need an instantiation of the template-specialization which is named as
+    // "opaque"
+    C4<float> inst_C4;
+
+    // the same, ust with two template-arguments -- for funz
+    template<typename T, typename U>
+    class C5 {
+        T* another_float_pointer;
+        U* just_more_members;
+    };
+    // ...instantiation
+    C5<float,int> inst_C5;
 
 }
 
