fb843dbf162c2cab9a8dc3358fef68fba78c70cc
Martin Zenzes <martin.zenzes@dfki.de>
2014-12-09 14:09:33 +0100

    typelib-tlb-import/clang-rb: use Tempfile rather than actual files
    also add some comments
    
    Signed-off-by: Martin Zenzes <martin.zenzes@dfki.de>

--- bindings/ruby/lib/typelib/clang.rb
+++ bindings/ruby/lib/typelib/clang.rb
@@ -6,47 +6,61 @@ module Typelib
     class Registry
         # Imports the given C++ file into the registry using CLANG
         def self.load_from_clang(registry, file, kind, options)
+
+            # FIXME: the second argument "file" contains the preprocessed
+            # output created in an earlier stage. it is not used here, but the
+            # list of actual header-files in the "options" hash. not having to
+            # pass 100k likes of text might improve performance?
             
-            #this gives us an array of opaques
+            # this gives us an array of opaques
             opaques = options[:opaques]
             
-            #create a registry from it
-            # FIXME: instead of usign the "Tempfile", just use a fixed known file in the rock-install for human inspection. to be removed!
-            #opaqueReg = Tempfile.open("tmp_opaques")
-            opaqueReg = File.open("#{ENV["AUTOPROJ_CURRENT_ROOT"]}/opaques", "w+")
+            # create a registry from the given opaques, so that the importer can
+            # load the informations to use it sduring processing.
+            #
+            # FIXME: reuse some code for creating a "opaque.tlb"
+            opaqueReg = Tempfile.open("tlb-import-opaques-")
             opaqueReg << "<typelib>"
             opaques.each do |opaque|
-                typedef = "<opaque name=\"#{opaque.gsub('<', '&lt;').gsub('>', '&gt;')}\" size=\"#{0}\" />"
-                opaqueReg << typedef
+                opaqueReg << "<opaque name=\"#{opaque.gsub('<', '&lt;').gsub('>', '&gt;')}\" size=\"0\" />"
             end
             opaqueReg << "</typelib>"
             opaqueReg.flush
             
-            # FIXME: see above
-            tmpReg = File.open("#{ENV["AUTOPROJ_CURRENT_ROOT"]}/registry", "w+")
+            # a file where the importer can store its results for merging into
+            # our database
+            tmpReg = Tempfile.open("tlb-save-registry-")
             
             include_dirs = options[:include_paths]
-            include_path = include_dirs.map { |d| "-I #{d}" }.join(" ")
+            include_path = include_dirs.map { |d| "-I#{d}" }.join(" ")
 
+            # which files actually to operate on
+            #
+            # FIXME: calling the importer on this list of files can be
+            # parallelized?
             header_files = options[:required_files].join(" ") 
             
-            # calling the the installed töb-import-tool, found in the
+            # calling the the installed tlb-import-tool, hopefully found in the
             # install-folder via PATH.
             finalCmd = "typelib-clang-tlb-importer -opaquePath=\"#{opaqueReg.path}\" -tlbSavePath=\"#{tmpReg.path}\" #{header_files} -- #{include_path} -x c++"
 
-            # call extractor tool
+            # and finally call importer-tool
             retval = system(finalCmd)
             if retval != true
                 raise InternalError, "typelib-clang-tlb-importer failed!"
             end
             
+            # read the registry created by the tool back into this ruby process
             result = Registry.from_xml(tmpReg.read())
-            
+            # and merge it into our registry
             registry.merge(result)
         end
         TYPE_HANDLERS['c'] = method(:load_from_clang)
     end
 
+    # some functions copied fs-is rom the original implementation of
+    # "gccxml.rb" for reusing. they do string-manipulation and convenience
+    # stuff on cxx-names.
     class ClangLoader
         def self.parse_template(name)
             tokens = template_tokenizer(name)
