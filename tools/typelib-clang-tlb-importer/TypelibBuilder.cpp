1ad688cc53bcdacf8df660008e8e31f4dcab9ea1
Martin Zenzes <martin.zenzes@dfki.de>
2015-03-03 12:29:47 +0100

    clang: fix comment
    made obsolete by previous commit
    
    see fd80867189c23ca3c7ca438ff4c86c915051ab0b
    
    Signed-off-by: Martin Zenzes <martin.zenzes@dfki.de>

--- tools/typelib-clang-tlb-importer/TypelibBuilder.cpp
+++ tools/typelib-clang-tlb-importer/TypelibBuilder.cpp
@@ -180,9 +180,7 @@ TypelibBuilder::checkRegisterContainer(const std::string &canonicalTypeName,
               << "' successfully registered for Type '" << canonicalTypeName
               << "'\n";
 
-    // FIXME: and, as usual: all the metadata should not be missing? but we
-    // only get a const-reference from the container-factory... so ask the
-    // registry to get the type-pointer of the actual type... meh...
+    // setting the metadata
     setMetaDataDoc(decl, &newContainer);
     setMetaDataSourceFileLine(decl, &newContainer);
     setMetaDataCxxname(decl, &newContainer);
