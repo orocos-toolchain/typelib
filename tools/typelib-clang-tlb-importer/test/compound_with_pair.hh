25fcf9c8d124c5ef31529f9c8afc494591dfe861
Martin Zenzes <martin.zenzes@dfki.de>
2015-03-04 10:40:05 +0100

    clang: add an additional test-case using "std::pair"
    just found it on my system during cleanup... the "std::pair<>" is added
    as a "Compound" to the database.
    
    Signed-off-by: Martin Zenzes <martin.zenzes@dfki.de>

---
+++ tools/typelib-clang-tlb-importer/test/compound_with_pair.hh
@@ -0,0 +1,15 @@
+#ifndef TEST_HEADER_COMPOUND_WITH_PAIR
+#define TEST_HEADER_COMPOUND_WITH_PAIR
+
+#include <utility>
+
+namespace ns_compound_with_pairs {
+
+// lets see how this will work out...
+class TestClass {
+  public:
+    std::pair<double, double> member;
+};
+}
+
+#endif /*TEST_HEADER_DATA_CLASSES_H*/
