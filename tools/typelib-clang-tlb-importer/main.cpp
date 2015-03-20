0754deb52ca98d2703be3cb642dec88124b99af1
Martin Zenzes <martin.zenzes@dfki.de>
2015-03-20 10:55:29 +0100

    clang: fix include headers
    angle brackets denote system headers
    
    Signed-off-by: Martin Zenzes <martin.zenzes@dfki.de>

--- tools/typelib-clang-tlb-importer/main.cpp
+++ tools/typelib-clang-tlb-importer/main.cpp
@@ -11,20 +11,20 @@
 
 // include {{{1
 #include <clang/Tooling/CommonOptionsParser.h>
-#include "clang/ASTMatchers/ASTMatchers.h"
-#include "clang/ASTMatchers/ASTMatchFinder.h"
-#include "clang/Basic/SourceManager.h"
-#include "clang/Frontend/FrontendActions.h"
-#include "clang/AST/ASTContext.h"
-#include "clang/AST/RecordLayout.h"
-#include "clang/AST/Type.h"
-#include "clang/Lex/Lexer.h"
-#include "clang/Tooling/CompilationDatabase.h"
-#include "clang/Tooling/Refactoring.h"
-#include "clang/Tooling/Tooling.h"
-#include "llvm/Support/CommandLine.h"
-#include "llvm/Support/MemoryBuffer.h"
-#include "llvm/Support/Signals.h"
+#include <clang/ASTMatchers/ASTMatchers.h>
+#include <clang/ASTMatchers/ASTMatchFinder.h>
+#include <clang/Basic/SourceManager.h>
+#include <clang/Frontend/FrontendActions.h>
+#include <clang/AST/ASTContext.h>
+#include <clang/AST/RecordLayout.h>
+#include <clang/AST/Type.h>
+#include <clang/Lex/Lexer.h>
+#include <clang/Tooling/CompilationDatabase.h>
+#include <clang/Tooling/Refactoring.h>
+#include <clang/Tooling/Tooling.h>
+#include <llvm/Support/CommandLine.h>
+#include <llvm/Support/MemoryBuffer.h>
+#include <llvm/Support/Signals.h>
 
 #include "TypelibBuilder.hpp"
 #include "NamingConversions.hpp"
