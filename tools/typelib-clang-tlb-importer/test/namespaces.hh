2d39fc6dc74ff1940302e8d9bcc5cc92a0d3be61
Martin Zenzes <martin.zenzes@dfki.de>
2015-03-20 17:15:24 +0100

    clang: add test-case to the header to show problems with "inline namespaces"
    hmhm... how to support this properly?
    
    to get a proper view of the ast of this example run
        clang-3.4 $AUTOPROJ_CURRENT_ROOT/tools/typelib/tools/typelib-clang-tlb-importer/test/namespaces.hh -std=c++11 -Xclang -ast-dump -fsyntax-only
    
    Signed-off-by: Martin Zenzes <martin.zenzes@dfki.de>

--- tools/typelib-clang-tlb-importer/test/namespaces.hh
+++ tools/typelib-clang-tlb-importer/test/namespaces.hh
@@ -21,4 +21,27 @@ namespace N1 {
     }
 }
 
+// checking a c++11 related feature: "inline namespace", which is done in
+// "libc++" for example, used in OSX. to test this you can call the
+// importer directly and give it "-std=c++11" as additional argument
+// 
+// see http://en.cppreference.com/w/cpp/language/namespace#Inline_namespaces
+#if __cplusplus > 199711L
+
+namespace standard {
+    namespace __1 {
+        // define the class inside some "temporary" namespace
+        class StandardClass { public: int standardMember; };
+    }
+    // and then pull its content into the "standard" namespace
+    using namespace __1;
+}
+// now I can create the following object:
+standard::StandardClass dut;
+// the tlb-file will have entries like "standard::__1::StandardClass". this is
+// what happens on OSX with their libc++: entries like "std::__1::vector" for
+// example while we have an opaques named "std::vector" and don't match this.
+
+#endif
+
 #endif /*TEST_DATA_HEADER_NAMESPACES_H*/
