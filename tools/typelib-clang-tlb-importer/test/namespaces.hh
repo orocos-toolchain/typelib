f3e93337cf9339eac2711fd869b47c1c5dce2dcf
Martin Zenzes <martin.zenzes@dfki.de>
2015-03-23 11:43:03 +0100

    clang: fix "inline namespace" testcase -- this is not a c++11 feature
    Signed-off-by: Martin Zenzes <martin.zenzes@dfki.de>

--- tools/typelib-clang-tlb-importer/test/namespaces.hh
+++ tools/typelib-clang-tlb-importer/test/namespaces.hh
@@ -21,13 +21,9 @@ namespace N1 {
     }
 }
 
-// checking a c++11 related feature: "inline namespace", which is done in
-// "libc++" for example, used in OSX. to test this you can call the
-// importer directly and give it "-std=c++11" as additional argument
-// 
+// test "inline namespace": is done in "libc++" for example, used in OSX.
+//
 // see http://en.cppreference.com/w/cpp/language/namespace#Inline_namespaces
-#if __cplusplus > 199711L
-
 namespace standard {
     namespace __1 {
         // define the class inside some "temporary" namespace
@@ -36,12 +32,11 @@ namespace standard {
     // and then pull its content into the "standard" namespace
     using namespace __1;
 }
-// now I can create the following object:
+// now I could create the following object (just for illustration, not needed
+// for tlb-generation)
 standard::StandardClass dut;
 // the tlb-file will have entries like "standard::__1::StandardClass". this is
 // what happens on OSX with their libc++: entries like "std::__1::vector" for
 // example while we have an opaques named "std::vector" and don't match this.
-
-#endif
 
 #endif /*TEST_DATA_HEADER_NAMESPACES_H*/
