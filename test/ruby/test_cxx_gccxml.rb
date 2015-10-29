3c279f801af9126fcecc80c6b38ac85277ab3d9c
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-10-29 14:38:35 -0200

    gccxml: move type name normalization as late as possible
    This applies to vectors and template type names which point to
    typedef'd, opaques or vectors

--- test/ruby/test_cxx_gccxml.rb
+++ test/ruby/test_cxx_gccxml.rb
@@ -13,13 +13,13 @@ class TC_CXX_GCCXML < Minitest::Test
 
     def test_cxx_to_typelib_subtype_of_container
         cxx_name = 'std::vector <std::vector <int ,std::allocator<int>>, std::allocator< std::vector<int , std::allocator <int>>      > >::size_t'
-        assert_equal '/std/vector</std/vector</int>>/size_t',
+        assert_equal '/std/vector</std/vector</int,/std/allocator</int>>,/std/allocator</std/vector</int,/std/allocator</int>>>>/size_t',
             Typelib::GCCXMLLoader.cxx_to_typelib(cxx_name)
     end
 
     def test_cxx_to_typelib_template_of_container
         cxx_name = 'BaseTemplate<std::vector<double, std::allocator<double> > >'
-        assert_equal '/BaseTemplate</std/vector</double>>',
+        assert_equal '/BaseTemplate</std/vector</double,/std/allocator</double>>>',
             Typelib::GCCXMLLoader.cxx_to_typelib(cxx_name)
     end
 end
