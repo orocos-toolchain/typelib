f74a7350251dfb5085f35157d24026526d394d3f
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2016-08-18 11:34:48 -0300

    test: honor the ROCK_TEST_ENABLED and ROCK_TEST_LOG_DIR parameters
    While we don't want to import the CMake macros from Rock for the benefit
    of the Orocos guys, there's no harm in reducing the complexity on our
    side a bit.

--- test/ruby/cxx_common_tests.rb
+++ test/ruby/cxx_common_tests.rb
@@ -13,7 +13,7 @@ module CXXCommonTests
     end
 
     def setup_loader(loader_name, name: loader_name, **options)
-        if test_selected_loader = ENV['TEST_CXX_LOADER']
+        if test_selected_loader = ENV['TYPELIB_CXX_LOADER']
             if test_selected_loader != loader_name
                 skip "test environment selected #{test_selected_loader} as loader, skipping #{loader_name}"
             end
